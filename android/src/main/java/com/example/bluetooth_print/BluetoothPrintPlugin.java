fba6f4d615d056405803bd5aaf74fea73b53941a
zwtzwx <zwt384500364>
2022-10-25 17:54:57 +0800

    update

--- android/src/main/java/com/example/bluetooth_print/BluetoothPrintPlugin.java
+++ android/src/main/java/com/example/bluetooth_print/BluetoothPrintPlugin.java
@@ -176,35 +176,14 @@ public class BluetoothPrintPlugin implements FlutterPlugin, ActivityAware, Metho
         break;
       case "getDevices":
       {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-
-          if (ContextCompat.checkSelfPermission(activity,
-                  Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED ||
-                  ContextCompat.checkSelfPermission(activity,
-                          Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED ||
-                  ContextCompat.checkSelfPermission(activity,
-                          Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(activity,new String[]{
-                    Manifest.permission.BLUETOOTH_SCAN,
-                    Manifest.permission.BLUETOOTH_CONNECT,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-            }, 1);
-
-            pendingResult = result;
-            break;
-          }
-        } else {
-          if (ContextCompat.checkSelfPermission(activity,
-                  Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED||ContextCompat.checkSelfPermission(activity,
-                  Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            ActivityCompat.requestPermissions(activity,
-                    new String[] { Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION }, REQUEST_COARSE_LOCATION_PERMISSIONS);
-
-            pendingResult = result;
-            break;
-          }
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+          ActivityCompat.requestPermissions(
+                  activityBinding.getActivity(),
+                  new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                  REQUEST_FINE_LOCATION_PERMISSIONS);
+          pendingCall = call;
+          pendingResult = result;
+          break;
         }
         getDevices(result);
 
